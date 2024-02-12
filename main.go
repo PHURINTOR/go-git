5b3ccd2402bbf330d82f09404b2b389fee2a2677
PHURIN\PHURIN <phurin.daphuaphan@gmail.com>
2024-02-13 00:50:05 +0700

    sum complated

--- main.go
+++ main.go
@@ -5,4 +5,10 @@ import "fmt"
 func main() {
 	fmt.Println("This is Test git-go")
 	fmt.Println("This is Test git-go fix")
+	sum := sum(5, 4)
+	fmt.Printf("sum : %v", sum)
+}
+
+func sum(a, b int) int {
+	return a + b
 }
